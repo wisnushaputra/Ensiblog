e35ceb3ef638d1d6047d7283f4ba44a933b2a29b
unknown <wisnushaputra>
2024-01-11 03:17:35 +0700

    upload

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+//Toogle dan nav
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+    
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
